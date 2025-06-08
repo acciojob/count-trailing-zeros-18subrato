cd57e2398f643ae952b141dab018a69a134d0c3a
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-06-08 15:53:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 function trailingZeros(n) {
-	n = parseInt(n);
-	let fact = 1;
-	for(let i=1;i<=n;i++){
-		fact = fact * i;
-	}
-	let count = 0;
-	let str = String(fact);
-	for(let j=str.length-1,j>=0;j--){
-		if(str[j]==0){
-			count++;
-		} else {
-			break;
-		}
-	}
-	console.log(count);
+    n = parseInt(n);
+    let count = 0;
+    for (let i = 5; n / i >= 1; i *= 5) {
+        count += Math.floor(n / i);
+    }
+    return count;
 }
 
 const input = prompt('Enter a number');
